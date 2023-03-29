ecb9ddd6e62e859db04a4fb48c76a1f871ad786e
tngudzzz <63445173+tngudzzz@users.noreply.github.com>
2023-03-29 17:33:58 +0900

    2번 프로젝트 - 태마 관리
    제 앱에서 사용하는 태마 데이터 관리 파일입니다.

---
+++ theme_data.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Mycolor {
+  // 기본 폰트
+  String font = "Pretendard";
+  // 기본 배경화면 검정
+  Color backgroundDark = const Color(0xff1E1E1E);
+  // 기본 배경화면 회색
+  Color backgroundGrey = const Color(0xff2B2B2B);
+  // 글씨색
+  Color textwhite = const Color(0xfff9f9fc);
+  // 글씨색
+  Color textwhite60 = Colors.white60;
+  // 글씨색
+  Color textwhite70 = Colors.white70;
+  // 컨셉 색
+  Color basicRed = const Color(0xffff7360);
+  // 완료 색
+  Color basicGreen = const Color(0xff8FB339);
+  // 미루기 색
+  Color basicBlue = const Color(0xff3EB8FF);
+  // 스낵바 텍스트 색
+  Color snackbarText = Colors.white;
+  // 스낵바 아이콘 색
+  Color snackbarIcon = Colors.white;
+  // 삭제 버튼 색
+  Color delete = const Color(0xffE63946);
+}
